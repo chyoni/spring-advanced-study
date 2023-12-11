ee12cb4402b2ca304da0105aec31b4dc393e5d8b
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-12-11 19:38:15 +0900

    로그 추적기 만들기 Basic

---
+++ src/test/java/com/example/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,24 @@
+package com.example.advanced.trace.hellotrace;
+
+import com.example.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
