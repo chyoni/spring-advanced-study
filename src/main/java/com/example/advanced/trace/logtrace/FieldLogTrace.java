6a4fba076f14fa1445abdd16c788d5e788618c76
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-12-11 21:56:32 +0900

    동시성 문제의 원인

--- src/main/java/com/example/advanced/trace/logtrace/FieldLogTrace.java
+++ src/main/java/com/example/advanced/trace/logtrace/FieldLogTrace.java
@@ -15,6 +15,7 @@ public class FieldLogTrace implements LogTrace {
      * 동시성 이슈 발생.
      * 왜 동시성 이슈가 발생할까? 이유는 이 LogTrace라는 인터페이스의 구현체로 이 FieldLogTrace 직접 빈으로 등록했던것을 기억하는가?
      * 스프링 빈은 기본이 싱글톤으로 JVM내 단 한개의 인스턴스만 존재하게 된다. 근데 이 인스턴스를 동시에 여러개의 쓰레드가 접근하면 같은 필드(traceIdHolder)를 공유하기 때문에 발생한다.
+     * 즉, 하나의 쓰레드가 본인이 처리할 일을 다 처리하고 깔끔히 쓰레드를 반납(종료)하기 전 다른 쓰레드가 해당 필드에 접근하는 경우가 생기면 그게 동시성 문제이다.
      * */
     private TraceId traceIdHolder; // traceId 동기화
 
