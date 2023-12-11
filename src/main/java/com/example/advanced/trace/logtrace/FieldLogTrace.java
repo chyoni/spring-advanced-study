0d540b5777296d26867d32a9e167882baa7f09d3
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-12-11 21:41:55 +0900

    로그 추적기 만들기 (Field Synchronization)

---
+++ src/main/java/com/example/advanced/trace/logtrace/FieldLogTrace.java
@@ -0,0 +1,83 @@
+package com.example.advanced.trace.logtrace;
+
+import com.example.advanced.trace.TraceId;
+import com.example.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldLogTrace implements LogTrace {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    /**
+     * 동시성 이슈 발생.
+     * 왜 동시성 이슈가 발생할까? 이유는 이 LogTrace라는 인터페이스의 구현체로 이 FieldLogTrace 직접 빈으로 등록했던것을 기억하는가?
+     * 스프링 빈은 기본이 싱글톤으로 JVM내 단 한개의 인스턴스만 존재하게 된다. 근데 이 인스턴스를 동시에 여러개의 쓰레드가 접근하면 같은 필드(traceIdHolder)를 공유하기 때문에 발생한다.
+     * */
+    private TraceId traceIdHolder; // traceId 동기화
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+
+        TraceId traceId = traceIdHolder;
+
+        Long startTimeMs = System.currentTimeMillis();
+
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+
+        if (e == null) {
+            log.info("[{}] {}{} spentTime={}ms",
+                    traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} spentTime={}ms ex={}",
+                    traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+
+        releaseTraceId();
+    }
+
+    private void syncTraceId() {
+        if (traceIdHolder == null) {
+            traceIdHolder = new TraceId();
+        } else {
+            traceIdHolder = traceIdHolder.createNextId();
+        }
+    }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) {
+            traceIdHolder = null;
+        } else {
+            traceIdHolder = traceIdHolder.createPrevId();
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+}
